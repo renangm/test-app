cef9f14371c146a08e1e26711268e8afdc703299
LeonardoCoutin <40408797+LeonardoCoutin@users.noreply.github.com>
2019-04-08 21:48:47 -0300

    Create tabs-details.module.ts

---
+++ src/app/tabs-details/tabs-details.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { TabsDetailsPage } from './tabs-details.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsDetailsPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [TabsDetailsPage]
+})
+export class TabsDetailsPageModule {}
