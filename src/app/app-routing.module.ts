6037aabf8d7deef5f0b1b3abb40ffbd2650112ea
LeonardoCoutin <40408797+LeonardoCoutin@users.noreply.github.com>
2019-04-08 21:48:33 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'tabs1-details', loadChildren: './tabs1-details/tabs1-details.module#Tabs1DetailsPageModule' },
+  { path: 'tabs-details', loadChildren: './tabs-details/tabs-details.module#TabsDetailsPageModule' },
+  { path: 'tabs2-details', loadChildren: './tabs2-details/tabs2-details.module#Tabs2DetailsPageModule' },
+  { path: 'tabs3-details', loadChildren: './tabs3-details/tabs3-details.module#Tabs3DetailsPageModule' }
 ];
 @NgModule({
   imports: [
