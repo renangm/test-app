79fa6d90f585ca1263a5760fa8c4b875d145e5a4
LeonardoCoutin <40408797+LeonardoCoutin@users.noreply.github.com>
2019-04-08 21:49:41 -0300

    Create tabs-details.page.ts

---
+++ src/app/tabs-details/tabs-details.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs-details',
+  templateUrl: './tabs-details.page.html',
+  styleUrls: ['./tabs-details.page.scss'],
+})
+export class TabsDetailsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
